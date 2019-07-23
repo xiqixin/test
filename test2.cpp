f81c6756ee104077866c8507519020903ed20a07
xiqixin <51395131+xiqixin@users.noreply.github.com>
2019-07-23 15:25:21 +0800

    上传文件
    第一次从windows上传文件到git

---
+++ test2.cpp
@@ -0,0 +1,20 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+////输出乘法口诀表
+//int main()
+//{   
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i <= 9; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			int k = 0;
+//			k = i * j;
+//			printf("%d*%d=%2d ", i, j, k);
+//		}
+//		printf("\n");
+//	}
+//	system("pause");
+//	return 0;
+//}
