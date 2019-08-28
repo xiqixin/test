582ab4b8bc39f2fc35666b01558807dd8cef3690
xiqixin <51395131+xiqixin@users.noreply.github.com>
2019-08-28 21:52:50 +0800

    Add files via upload

---
+++ test2.c
@@ -0,0 +1,17 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main()
+//{
+//	char killer = 'a';
+//	for (killer = 'A'; killer <= 'D'; killer++)
+//	{
+//		if (((killer != 'A') + (killer == 'C') + (killer == 'D') + (killer != 'D')) == 3)
+//		{
+//			printf("%c\n", killer);
+//			break;
+//		}
+//	}
+//	system("pause");
+//	return 0;
+//}
